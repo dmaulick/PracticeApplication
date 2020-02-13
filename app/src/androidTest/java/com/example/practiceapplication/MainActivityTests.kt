d00a7a2ef298d1abf243674f464bc1d5d2b6adbd
David Maulick <david.e.maulick@pwc.com>
2020-02-12 19:02:26 -0800

    Early commit No comment

--- app/src/androidTest/java/com/example/practiceapplication/MainActivityTests.kt
+++ app/src/androidTest/java/com/example/practiceapplication/MainActivityTests.kt
@@ -3,11 +3,15 @@ package com.example.practiceapplication
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTests {
+
+//    @Rule
+//    val activityRule = ActivityScenarioRule<MainActivity>()
 
     @Test
     fun useAppContext() {
---
+++ app/src/main/java/com/example/practiceapplication/DrinkListFragment.kt
@@ -0,0 +1,30 @@
+package com.example.practiceapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DrinkListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.drink_list_fragment, null)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 1) Connect adapters
+
+        // 2) Initialize view properties, set click listeners, etc.
+
+        // 3) Subscribe to state
+    }
+
+}
